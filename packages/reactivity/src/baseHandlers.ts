b1e61ff5a955d3ad72f93e0f8b25abfd266111a3
artsmp <603703819@qq.com>
2022-07-03 01:10:28 +0800

    feat: 基础的依赖收集和写值更新

--- packages/reactivity/src/baseHandlers.ts
+++ packages/reactivity/src/baseHandlers.ts
@@ -1,3 +1,5 @@
+import { track, trigger } from "./effect";
+
 export const enum ReactiveFlags {
   /** 判断是否已经被代理过 */
   IS_REACTIVE = "__v_isReactive",
@@ -9,9 +11,17 @@ export const mutableHandler = {
     if (key === ReactiveFlags.IS_REACTIVE) {
       return true;
     }
+    // 取值时依赖收集
+    track(target, "get", key);
     return Reflect.get(target, key, receiver);
   },
   set(target, key, newVal, receiver) {
-    return Reflect.set(target, key, newVal, receiver);
+    // 写值时触发更新：注意这里书写顺序问题
+    const oldValue = target[key];
+    const result = Reflect.set(target, key, newVal, receiver);
+    if (oldValue !== newVal) {
+      trigger(target, "set", key, newVal, oldValue);
+    }
+    return result;
   },
 };
--- packages/reactivity/src/effect.ts
+++ packages/reactivity/src/effect.ts
@@ -1,21 +1,77 @@
-export let activeEffect = undefined;
+export let activeEffect: ReactiveEffect = undefined;
 class ReactiveEffect {
   public active = true;
+  public parent = null;
+  public deps = [];
   constructor(public fn: Function) {}
   run() {
     // 不是激活的effect，则只执行不进行依赖收集
     if (!this.active) this.fn();
     // 进行依赖收集
     try {
+      this.parent = activeEffect; // 记录自己的父亲
       activeEffect = this;
       this.fn();
     } finally {
       activeEffect = undefined;
+      activeEffect = this.parent; // 自己执行完后重新设置自己的父亲为 activeEffect
+      this.parent = null;
     }
   }
 }
 
 export const effect = (fn: Function) => {
   const _effect = new ReactiveEffect(fn);
-  _effect.run();
+  _effect.run(); // 默认先执行一次
 };
+
+const targetMap = new WeakMap(); // 依赖存放地：键为对象，值为Map，Map里面的键为对象的key，值为Set，Set存放的是effect实例
+/**
+ * 收集依赖
+ * @param target 要进行收集的源对象
+ * @param type 类型：get
+ * @param key 访问的对象 key
+ * 最终的数据结构 targetMap => WeakMap { target: Map { key: Set [ effect ] } }
+ */
+export const track = (target, type, key) => {
+  if (!activeEffect) return;
+  let depsMap = targetMap.get(target);
+  if (!depsMap) {
+    targetMap.set(target, (depsMap = new Map()));
+  }
+  let dep = depsMap.get(key);
+  if (!dep) {
+    depsMap.set(key, (dep = new Set()));
+  }
+  // 这么做有一定的性能优化效果
+  let shouldTrack = dep.has(activeEffect);
+  if (!shouldTrack) {
+    dep.add(activeEffect);
+    activeEffect.deps.push(dep); // 进行反向收集依赖，清除副作用时会用到
+  }
+};
+
+export const trigger = (target, type, key, newVal, oldVal) => {
+  const depsMap = targetMap.get(target);
+  if (!depsMap) return;
+  const effects = depsMap.get(key);
+  effects &&
+    effects.forEach((effect) => {
+      if (effect !== activeEffect) {
+        // 防止在effect回调中再次修改目标属性值，造成爆栈
+        effect.run();
+      }
+    });
+};
+
+/* 
+嵌套使用effect的情况处理：为每个effect新增一个 parent 属性，记录自己的父亲，自己执行完毕后将父亲再修改为activeEffect
+effect(() => {
+  state.name
+  effect(() => {
+    state.age
+  })
+  state.address
+})
+
+*/
